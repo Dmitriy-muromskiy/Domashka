b9ee8b7b3dd2505ae562ea6d91d6f4075870f827
79206 <serdukovd.a.murom@yandex.ru>
2024-03-10 17:51:36 +0300

    firs task

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,18 @@ public class Main {
         short s = 777;
         int i = 25464;
         long l = 64646L;
-        System.out.println("Значение переменной");
+        float f = -46.57647f;
+        double d = 647.6353555626;
+        System.out.println("Значение переменной b с типом переменной byte равно " + b);
+        System.out.println("Значение переменной s с типом переменной short равно " + s);
+        System.out.println("Значение переменной i с типом переменной int равно " + i);
+        System.out.println("Значение переменной l с типом переменной long равно " + l);
+        System.out.println("Значение переменной f с типом переменной float равно " + f);
+        System.out.println("Значение переменной d с типом переменной double равно " + d);
+
+        //задание №2
+
+
 
 
     }
